1a16f8b4a36525d91c39484ff44f9ac4bc200b10
Juan David Pineda Sanchez <jpineda@superricas.com>
2025-02-05 21:41:45 -0500

    Creación de función para añadir nombre y para insertar lista de nombres ingresados

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const names = [];
+
+const buttonAdd = document.querySelector(".button-add");
+
+buttonAdd.addEventListener('click', function() {
+    const inputFriend = document.querySelector(".input-name").value;
+    if (!inputFriend) {
+        alert("Debe ingresar un nombre")
+    } else {
+        alert(`El nombre ${inputFriend} fue agregado correctamente`)
+        names.push(inputFriend);
+    }  
+    listAdd()
+    document.querySelector(".input-name").value = "";
+})
+
+const listAdd =()=>{
+    const list = document.querySelector(".name-list"); 
+    list.innerHTML = "";
+
+    for (let i = 0; i < names.length; i++) {
+        
+        const element = document.createElement("li")
+         element.textContent = names[i];
+
+         list.appendChild(element);
+    }
+}
+
+
+
